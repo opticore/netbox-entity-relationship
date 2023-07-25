466f54584e90d25547786b9e988fd9d555a0f258
apishark <james.curtis@opticoreit.com>
2023-07-25 11:22:56 +0100

    fix_form_widget

--- netbox_entity_relationship/forms.py
+++ netbox_entity_relationship/forms.py
@@ -5,7 +5,8 @@ from netbox_entity_relationship.choices import AppChoices as APPCHOICES
 from netbox_entity_relationship.choices import ArrowShapeChoices as ARROWSHAPECHOICES
 from netbox_entity_relationship.choices import OptionsChoices as OPTIONSCHOICES
 from netbox_entity_relationship.choices import RankDirChoices as RANKDIROPTIONSCHOICES
-from utilities.forms import DynamicModelChoiceField, StaticSelect, add_blank_choice
+from utilities.forms import add_blank_choice
+from utilities.forms.fields import DynamicModelChoiceField
 from utilities.forms.widgets import APISelect
 
 
@@ -16,7 +17,7 @@ class EntityRelationshipForm(forms.Form):
         choices=add_blank_choice(APPCHOICES),
         required=False,
         help_text="Select an App",
-        widget=StaticSelect(),
+        widget=forms.Select(),
     )
     model_name = DynamicModelChoiceField(
         queryset=ContentType.objects.all(),
@@ -31,35 +32,35 @@ class EntityRelationshipForm(forms.Form):
         initial=ARROWSHAPECHOICES.NORMAL,
         required=False,
         help_text="Arrow shape to use for relations",
-        widget=StaticSelect(),
+        widget=forms.Select(),
     )
     color_code_deletions = forms.ChoiceField(
         choices=OPTIONSCHOICES,
         initial=OPTIONSCHOICES.FALSE,
         required=False,
         help_text="Color the relations according to their on_delete setting, where it it applicable. The colors are: red (CASCADE), orange (SET_NULL), green (SET_DEFAULT), yellow (SET), blue (PROTECT), grey (DO_NOTHING) and purple ",
-        widget=StaticSelect(),
+        widget=forms.Select(),
     )
     hide_edge_labels = forms.ChoiceField(
         choices=OPTIONSCHOICES,
         initial=OPTIONSCHOICES.FALSE,
         required=False,
         help_text="Do not show relations labels in the graph",
-        widget=StaticSelect(),
+        widget=forms.Select(),
     )
     rankdir = forms.ChoiceField(
         choices=RANKDIROPTIONSCHOICES,
         initial=RANKDIROPTIONSCHOICES.BT,
         required=False,
         help_text="Set direction of graph layout",
-        widget=StaticSelect(),
+        widget=forms.Select(),
     )
     relation_fields_only = forms.ChoiceField(
         choices=OPTIONSCHOICES,
         initial=OPTIONSCHOICES.FALSE,
         required=False,
         help_text="Only display fields that are relevant for relations",
-        widget=StaticSelect(),
+        widget=forms.Select(),
     )
 
     fieldsets = (
